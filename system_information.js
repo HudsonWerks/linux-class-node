02a98e8fe64562d79d1a167a042e3591a04ab70c
Charles Hamilton <HudsonWerks@users.noreply.github.com>
2017-05-03 15:13:22 -0400

    Update system_information.js

--- system_information.js
+++ system_information.js
@@ -5,7 +5,3 @@ si.cpu(function(data) {
     console.log('CPU-Information:');
     console.log(data);
 })
- 
-si.cpu()
-    .then(data => console.log(data))
-    .catch(error => console.error(error));
